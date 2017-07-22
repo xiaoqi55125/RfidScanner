e7ba5032303d4048ac0dff7bee56fc5963b9a166
Zhicheng Liu <zhicheng@abcve.com>
2017-07-22 15:29:56 +0800

    1

--- src/android/RfidScanner.java
+++ src/android/RfidScanner.java
@@ -1,37 +1,72 @@
 package com.pigwell.cordova.plugin;
 
-// import android.app.Activity;
-// import android.content.Intent;
-// import android.util.Log;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 
-// import com.android.hdhe.uhf.reader.Tools;
-// import com.android.hdhe.uhf.reader.UhfReader;
+import com.android.hdhe.uhf.reader.Tools;
+import com.android.hdhe.uhf.reader.UhfReader;
 
-// import org.apache.cordova.CallbackContext;
-// import org.apache.cordova.CordovaPlugin;
-// import org.json.JSONArray;
-// import org.json.JSONException;
-// import org.json.JSONObject;
-// import java.util.List;
-// import android.widget.Toast;
-// import java.util.ArrayList;
-// import org.apache.cordova.PluginResult;
-import org.apache.cordova.*;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.app.Activity;
-import android.os.Bundle;
+import org.json.JSONObject;
+import java.util.List;
+import android.widget.Toast;
+import java.util.ArrayList;
+import org.apache.cordova.PluginResult;
+
 /**
  * Created by avila on 28/10/13.
  */
 public class RfidScanner extends CordovaPlugin {
- 
+    public static final int SCAN_CODE = 0;
+    public static final int RADAR_CODE = 1;
+    private static final String SCAN_INTENT = "eficid.intent.action.RFID_SCAN";
+    private static final String RADAR_INTENT = "eficid.intent.action.RFID_RADAR";
+    private static final String CANCELLED = "cancelled";
+    private static final String LOG_TAG = "RfidScanner";
+    private UhfReader uhfReader;
+    private boolean startFlag = false;
+    private List<byte[]> epcList;
+    String epcStr = null;
 
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("scan")) {
+            // startFlag = true;
+            // uhfReader = UhfReader.getInstance();
+            // epcList = uhfReader.inventoryRealTime(); //实时盘存
+            // if (epcList != null && !epcList.isEmpty()) {
+            //     //扫描到后立即关闭连接,防止多次beep
+            //     if (uhfReader != null) {
+            //         uhfReader.close();
+            //     }
+            //     for (byte[] epc : epcList) {
+            //         epcStr = Tools.Bytes2HexString(epc, epc.length);
+            //     }
+            //     // Intent calIntent = new Intent(Intent.ACTION_EDIT)
+            //     //         .putExtra("tfidid", epcStr);
+            //     // this.cordova.getActivity().startActivity(calIntent);
+            //     JSONObject json = new JSONObject();
+            //     try{
+            //         json.put("type", "1");
+            //         json.put("signin", "2");
+            //     }catch(JSONException ex){
+            //     }
+                // PluginResult mPlugin = new PluginResult(PluginResult.Status.OK,json);  
+                // mPlugin.setKeepCallback(true);  
+                // callbackContext.sendPluginResult(mPlugin);  
+                // callbackContext.success();
+                String message = "1111111112 ";
+                callbackContext.success(message);
 
-        if (action.equals("greet")) {
+                return true;
+            // }
+        }  
+         if (action.equals("greet")) {
 
             String name = data.getString(0);
             String message = "Hello, " + name ;
@@ -44,6 +79,7 @@ public class RfidScanner extends CordovaPlugin {
             return false;
 
         }
+        return true;
     }
 
  
--- www/rfidscanner.js
+++ www/rfidscanner.js
@@ -1,5 +1,8 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "RfidScanner", "greet", [name]);
+    },
+    scan: function ( successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "RfidScanner", "scan",[]);
     }
 };
