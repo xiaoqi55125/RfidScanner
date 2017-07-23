200d473ecf5bce694c1b245c75add05faf1c236e
Zhicheng Liu <zhicheng@abcve.com>
2017-07-23 12:57:33 +0800

    终于测试成功了~~~~

--- src/android/RfidScanner.java
+++ src/android/RfidScanner.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import org.apache.cordova.PluginResult;
 
 /**
- * Created by avila on 28/10/13.
+ * Created by zhicheng
  */
 public class RfidScanner extends CordovaPlugin {
     public static final int SCAN_CODE = 0;
@@ -31,66 +31,40 @@ public class RfidScanner extends CordovaPlugin {
     private boolean startFlag = false;
     private List<byte[]> epcList;
     String epcStr = null;
+    private boolean runFlag = true;
 
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        // if (action.equals("greet")) {
-        //     cordova.getThreadPool().execute(new Runnable() {
-        //         public void run() {
-        //             startFlag = true;
-        //             uhfReader = UhfReader.getInstance();
-        //             epcList = uhfReader.inventoryRealTime(); //实时盘存
-        //             if (epcList != null && !epcList.isEmpty()) {
-        //                 //扫描到后立即关闭连接,防止多次beep
-        //                 if (uhfReader != null) {
-        //                     uhfReader.close();
-        //                 } 
-        //                 for (byte[] epc : epcList) {
-        //                     epcStr = Tools.Bytes2HexString(epc, epc.length);
-        //                 }
-        //                 String message = "1111111112"+epcStr;
-        //                 callbackContext.success(message);
-        //             }
-        //         });
-        //         return true;
-        //     }
-        // }else {
-        //     return false;
-        // }  
+
         if ("greet".equals(action)) {
-            Runnable runnable = new Runnable() {
-                public void run() {
-                     startFlag = true;
-                    
-                     try {
-                       Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), "123", Toast.LENGTH_SHORT);
-                        toast.show();
-                    } catch (Exception e) {
-                        Log.e("###",e.getMessage());
-                    }
-                    // String message = "1111111112";
-                    // callbackContext.success(message);
-                }
-            };
-            cordova.getActivity().runOnUiThread(runnable);
-            return true;
+             startFlag = true;
+             try {
+                 while (runFlag) {
+                     if (startFlag) {
+                         Log.e("###", "11111111111111111111");
+                         uhfReader = UhfReader.getInstance();
+                         epcList = uhfReader.inventoryRealTime(); //实时盘存
+                         if (epcList != null && !epcList.isEmpty()) {
+                             //扫描到后立即关闭连接,防止多次beep
+                             runFlag = false;
+                             if (uhfReader != null) {
+                                 uhfReader.close();
+                             }
+                             for (byte[] epc : epcList) {
+                                 epcStr = Tools.Bytes2HexString(epc, epc.length);
+                             }
+
+                         }
+                     }
+                 }
+                 callbackContext.success(epcStr);
+                 return true;
+            } catch (Exception e) {
+                Log.e("###",e.getMessage());
+            }
         }
         return false;
-        //  if (action.equals("greet")) {
-
-        //     String name = data.getString(0);
-        //     String message = "Hello, " + name ;
-        //     callbackContext.success(message);
-
-        //     return true;
-
-        // } else {
-            
-        //     return false;
-
-        // }
     }
 
- 
 }
